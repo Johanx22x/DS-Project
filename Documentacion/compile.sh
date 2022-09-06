1ddf0318e0a729840d1b479a1ea1b5d573321459
johanx22x <johan@dell.localhost>
2022-09-06 09:37:15 -0600

    Documentation configuration (Makefile, docs)

--- Documentacion/compile.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-pandoc docs/* -t pdf -s -o DocumentacionProyecto.pdf --metadata-file=meta.yaml --bibliography=references.bib --citeproc --toc --highlight-style=tango
-
---
+++ Documentacion/docs/3-Solucion.md
@@ -0,0 +1,1 @@
+# Solucion del problema 
