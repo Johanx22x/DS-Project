d4f828009a6b992a616e67ff428f143ef98e960a
Johanx22x <johanrodsa2210@gmail.com>
2022-09-24 15:52:18 -0600

    Tmp commit

--- Code/lib/main.cpp
+++ Code/lib/main.cpp
@@ -3,9 +3,30 @@
 #include <menu.hh>
 
 int main() {
+    // Principal menu definition
     Menu menu = Menu();
 
+    // Sub-menus definition
+    Menu dataManagement = Menu();
+    Menu consult = Menu();
+    Menu report = Menu();
+
+    /* auto lambda = [&dataManagement]() { */
+    /* auto lambda = []() { */
+    /*     dataManagement.display(); */
+    /*     while (dataManagement.prompt()); */
+    /*     return -1; */
+    /* }; */
+
     menu.addItem(new MenuItem(0, "Exit", []() {
+                return -1;
+                }));
+    /* menu.addItem(new MenuItem(1, "Data management", *lambda)); */
+
+    menu.addItem(new MenuItem(2, "Consults", []() {
+                return -1;
+                }));
+    menu.addItem(new MenuItem(3, "Reports", []() {
                 return -1;
                 }));
 
