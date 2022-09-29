43c9399c62db169eef13967ec8a354f5d9b07ee9
zSnails <erizojuan33@gmail.com>
2022-09-29 11:51:45 -0600

    Implement averageRainfall function and add min + max functions

--- Code/lib/rain.cpp
+++ Code/lib/rain.cpp
@@ -8,9 +8,60 @@ Rain::Rain(std::string name, std::string id, double rainfall) {
     this->rainfall = rainfall;
 }
 
+double Rain::min() {
+    Rain *tmp = this;
+    double mini = 0;
+    while (tmp != nullptr) {
+        if (tmp->rainfall < mini) mini = tmp->rainfall;
+        tmp = tmp->next;
+    }
+
+    return mini;
+}
+
+double Rain::max() {
+    Rain *tmp = this;
+    double maxi = 0;
+    while (tmp != nullptr) {
+        if (tmp->rainfall > maxi) maxi = tmp->rainfall;
+        tmp = tmp->next;
+    }
+
+    return maxi;
+}
+
 std::string Rain::averageRainfall() {
-    // TODO: implement this functionality
-    return "";
+    double average = 0;
+    int nodes = 0;
+    Rain *tmp = this;
+
+    double mini = this->min();
+
+    double maxi = this->max();
+
+    for (;tmp != nullptr; tmp = tmp->next) {
+        // normalization formula z1 = (x1 - min(x)) / (max(x) - min(x))
+        average += (tmp->rainfall - mini) / (maxi - mini);
+        nodes++;
+    }
+    
+    average = average / nodes;
+
+    std::string out;
+
+    if (average == 0) {
+        out = "extremo seco";
+    } else if (average < 0.2) {
+        out = "seco";
+    } else if (average < 0.4) {
+        out = "normal";
+    } else if (average < 0.8) {
+        out = "lluvioso";
+    } else {
+        out = "extremo lluvioso";
+    }
+
+    return out;
 }
 
 void Rain::show() {
